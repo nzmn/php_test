9d4826b5e5909dd9ec18ccb5acc51e9ae585a118
nzmn <36116668+nzmn@users.noreply.github.com>
2018-12-10 17:27:53 +1300

    Part 3: Logic test

--- foobar.php
+++ foobar.php
@@ -1,10 +1,11 @@
 <?php
 /**
+ * Part 3: Logic Test
  * Output the numbers from 1 to 100;
  * Where the number is divisible by three (3) output the word “foo”;
  * Where the number is divisible by five (5) output the word “bar”；
  * Where the number is divisible by three (3) and (5) output the word “foobar”;
- * User: Na Zhang
+ * Author: Na Zhang
  * Date: 2018-12-10
  * Time: 11:38 AM
  */
@@ -12,14 +13,17 @@ for ($i=1;$i<=100; $i++){
 
     switch($i)
     {
+        // the number is divisible by three (3) and (5) output the word “foobar”;
         case ($i%15==0):
             echo "foobar" . ", ";
             break;
 
+        // the number is divisible by three (3) output the word “foo”;
         case ($i%3==0):
             echo "foo" . ", ";
             break;
 
+        // the number is divisible by five (5) output the word “bar”；
         case ($i%5==0):
             echo "bar".", ";
             break;
